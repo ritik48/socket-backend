088e9ca524d7f6b57c36ba614cc225e46faf4bac
Ritik Raj <raj769417@gmail.com>
2024-04-29 18:23:51 +0530

    include timstamps in user

--- dist/models/User.js
+++ dist/models/User.js
@@ -14,6 +14,6 @@ const userSchema = new Schema({
     email: {
         type: String,
     },
-});
+}, { timestamps: true });
 const User = mongoose_1.default.model("user", userSchema);
 exports.User = User;
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,15 +2,18 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+        },
     },
-    email: {
-        type: String,
-    },
-});
+    { timestamps: true }
+);
 
 const User = mongoose.model("user", userSchema);
 
